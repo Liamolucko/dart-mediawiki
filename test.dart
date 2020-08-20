b5ba0a22a08e5ca337b4e590312225ce1bc67bef
Liamolucko <liampm32@gmail.com>
2020-08-20 17:02:33 +1000

    Remove test.dart

--- test.dart
+++
@@ -1,9 +0,0 @@
-import 'lib/wiki.dart';
-
-Future main() async {
-  final wiki = await Wiki.fromUrl('https://en.wikipedia.org/w/');
-  await for (var revision
-      in wiki.page('Jupiter').history(filter: 'bot', limit: 100)) {
-    print(revision.user.name);
-  }
-}
